8220fea0d14ea5a77d6587093b8e2921df94bc71
jeffdesouzadev <jeffdesouzadev@gmail.com>
2021-01-07 12:16:34 -0600

    Echo V2 - triggering a light on receipt of message

--- IGCTest/Program.cs
+++ IGCTest/Program.cs
@@ -21,77 +21,91 @@ namespace IngameScript
 {
     partial class Program : MyGridProgram
     {
-        // This file contains your actual script.
-        //
-        // You can either keep all your code here, or you can create separate
-        // code files to make your program easier to navigate while coding.
-        //
-        // In order to add a new utility class, right-click on your project, 
-        // select 'New' then 'Add Item...'. Now find the 'Space Engineers'
-        // category under 'Visual C# Items' on the left hand side, and select
-        // 'Utility Class' in the main area. Name it in the box below, and
-        // press OK. This utility class will be merged in with your code when
-        // deploying your final script.
-        //
-        // You can also simply create a new utility class manually, you don't
-        // have to use the template if you don't want to. Just do so the first
-        // time to see what a utility class looks like.
-        // 
-        // Go to:
-        // https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
-        //
-        // to learn more about ingame scripts.
+        /*
+ * This is MDK IGC Example 2 by wicorel
+ * 
+ * Takes the first example script of toggling a light and makes it work via IGC.
+ * 
+ * Go to:
+ *  https://github.com/malware-dev/MDK-SE/wiki/Quick-Introduction-to-Space-Engineers-Ingame-Scripts
+ *  to learn more about ingame scripts.
+ * 
+ * This Example source: 
+ *  https://github.com/Wicorel/WicoSpaceEngineers/tree/master/MDK%20IGC%20Example%202
+ */
 
-        int _runcount = 0;
-        string _broadCastTag = "MDK IGC EXAMPLE 1";
+        /// <summary>
+        /// This is our unique ID for our message.  We've defined the format for the message data (it's just a string)
+        /// </summary>
+        string _broadCastTag = "MDK IGC EXAMPLE 2";
+
+        /// <summary>
+        /// The broadcast listener for the channel we are interested in.
+        /// </summary>
         IMyBroadcastListener _myBroadcastListener;
 
         public Program()
         {
+            // let them know we are alive
             Echo("Creator");
+
+            // register a broadcast channel for our tag
             _myBroadcastListener = IGC.RegisterBroadcastListener(_broadCastTag);
-            _myBroadcastListener.SetMessageCallback(_broadCastTag);
+
+            // Ask to be called back --to Main()-- when a message is received
+            _myBroadcastListener.SetMessageCallback(_broadCastTag); // the callback agrument does NOT need to be the same as the tag
+
         }
+
 
         public void Main(string argument, UpdateType updateSource)
         {
-            _runcount++;
-            Echo(_runcount.ToString() + ":" + updateSource.ToString());
-
             if (
-                (updateSource & (UpdateType.Trigger | UpdateType.Terminal)) > 0
-                || (updateSource & (UpdateType.Mod)) > 0
-                || (updateSource & (UpdateType.Script)) > 0
+                (updateSource & (UpdateType.Trigger | UpdateType.Terminal)) > 0 // run by a terminal action
+                || (updateSource & (UpdateType.Mod)) > 0 // script run by a mod
+                || (updateSource & (UpdateType.Script)) > 0 // this pb run by another script (PB)
                 )
-            {
+            { // script was run because of an action
                 if (argument != "")
                 {
+                    // if we are given an argument, send it out over our broadcast channel
                     IGC.SendBroadcastMessage(_broadCastTag, argument);
                     Echo("Sending message:\n" + argument);
                 }
             }
 
             if ((updateSource & UpdateType.IGC) > 0)
-            {
+            { // script was run because of incoming IGC message
                 while (_myBroadcastListener.HasPendingMessage)
                 {
-                    MyIGCMessage myIGCMessage = _myBroadcastListener.AcceptMessage();
+                    var myIGCMessage = _myBroadcastListener.AcceptMessage();
                     if (myIGCMessage.Tag == _broadCastTag)
                     { // This is our tag
                         if (myIGCMessage.Data is string)
                         {
-                            string str = myIGCMessage.Data.ToString();
-                            Echo("Received IGC Public Message");
-                            Echo("Tag=" + myIGCMessage.Tag);
-                            Echo("Data=" + myIGCMessage.Data.ToString());
-                            Echo("Source=" + myIGCMessage.Source.ToString("X"));
+                            string lightName = myIGCMessage.Data.ToString();
+
+                            IMyInteriorLight light;
+
+                            light = GridTerminalSystem.GetBlockWithName(lightName) as IMyInteriorLight;
+                            if (light == null)
+                            {
+                                Echo("Oh my! I couldn't find that block...");
+                                Echo(lightName);
+                                return;
+                            }
+
+                            light.Enabled = !light.Enabled;
+                            Echo("I have toggled the light!");
                         }
                         else // if(msg.Data is XXX)
                         {
+                            // handle other data types here...
                         }
                     }
                     else
                     {
+                        // handle other tags here
                     }
                 }
             }
